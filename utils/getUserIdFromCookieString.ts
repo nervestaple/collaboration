286bfb8f92c67c27896f3b7358cb9380c42731ca
Jim McGee <nervestaple@gmail.com>
2022-10-03 12:48:10 -0400

    fix socket for https

--- utils/getUserIdFromCookieString.ts
+++ utils/getUserIdFromCookieString.ts
@@ -1,13 +1,16 @@
 import cookie from 'cookie';
 import getSessionAndUser from '../db/getSessionAndUser';
 
+const cookiePrefix = location.protocol.startsWith('https:') ? '__Secure-' : '';
+const tokenCookieKey = `${cookiePrefix}next-auth.session-token`;
+
 export default async function getUserIdFromCookieString(cookieString?: string) {
   if (!cookieString) {
     return null;
   }
 
   const cookies = cookie.parse(cookieString);
-  const sessionToken = cookies['next-auth.session-token'];
+  const sessionToken = cookies[tokenCookieKey];
   if (!sessionToken) {
     return null;
   }
